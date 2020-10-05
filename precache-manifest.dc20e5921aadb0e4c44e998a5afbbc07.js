af37302972b2f682ade40ca00fde803fc57f70b2
David Habl <habl@redblue.de>
2020-10-05 17:34:57 +0200

    Updates

---
+++ precache-manifest.dc20e5921aadb0e4c44e998a5afbbc07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6112f9291ad9ba9eb1257b6b9f36cab3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "91c4b6460c1c7fb0efa6",
+    "url": "/monsters-rolodex/static/css/main.3ee1d22e.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c4b6460c1c7fb0efa6",
+    "url": "/monsters-rolodex/static/js/main.258522d5.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
